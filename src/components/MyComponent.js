c5aef9f5e061d65b1e0dd32c9bc7dbb14fe4d25c
Thang Phan <phanthq2k2@gmail.com>
2024-11-08 17:04:30 +0700

    [MyComponent|16]: a first component

---
+++ src/components/MyComponent.js
@@ -0,0 +1,17 @@
+//class components
+//function components
+
+import React from "react";
+
+class MyComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        My first components
+        {Math.random()}
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
