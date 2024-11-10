d3c69a3d591800fd4a89f26cedf3267e3a986122
Thang Phan <phanthq2k2@gmail.com>
2024-11-10 22:53:16 +0700

    [MyComponent]: conditional output - câu điều kiện trong React

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,18 +1,40 @@
 import React from "react";
 
 class DisplayInfor extends React.Component {
+  state = {
+    isDisplayable: true,
+  };
+
+  handleShowHideListEvent = () => {
+    this.setState({ isDisplayable: !this.state.isDisplayable });
+  };
   render() {
     const { listUsers } = this.props;
     return (
       <div>
-        {listUsers.map((user) => {
-          return (
-            <div key={user.id}>
-              <div>My name is {user.name}</div>
-              <div>My age is {user.age}</div>
-            </div>
-          );
-        })}
+        <div>
+          <button
+            onClick={() => {
+              this.handleShowHideListEvent();
+            }}
+          >
+            {this.state.isDisplayable === true
+              ? "Hide List users"
+              : "Show List users"}
+          </button>
+        </div>
+        {this.state.isDisplayable && (
+          <div>
+            {listUsers.map((user) => {
+              return (
+                <div key={user.id} className={user.age > 18 ? "green" : "red"}>
+                  <div>My name is {user.name}</div>
+                  <div>My age is {user.age}</div>
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     );
   }
