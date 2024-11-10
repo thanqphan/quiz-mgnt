40ecb66c0c46c32b337420ee6c517675130d567b
Thang Phan <phanthq2k2@gmail.com>
2024-11-10 21:55:27 +0700

    [MyComponent|24]: render array/object

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -2,11 +2,17 @@ import React from "react";
 
 class DisplayInfor extends React.Component {
   render() {
-    const { name, age } = this.props;
+    const { listUsers } = this.props;
     return (
       <div>
-        <div>My name is {name}</div>
-        <div>My age is {age}</div>
+        {listUsers.map((user) => {
+          return (
+            <div key={user.id}>
+              <div>My name is {user.name}</div>
+              <div>My age is {user.age}</div>
+            </div>
+          );
+        })}
       </div>
     );
   }
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -6,12 +6,20 @@ import UserInfor from "./UserInfor";
 import DisplayInfor from "./DisplayInfor";
 
 class MyComponent extends React.Component {
+  state = {
+    listUsers: [
+      { id: 1, name: "Thang", age: 22 },
+      { id: 2, name: "Anh", age: 12 },
+      { id: 3, name: "Phan", age: 32 },
+      { id: 4, name: "Louis", age: 27 },
+    ],
+  };
   render() {
     return (
       <div>
         <UserInfor></UserInfor>
         <hr />
-        <DisplayInfor name="Louis" age={18}></DisplayInfor>
+        <DisplayInfor listUsers={this.state.listUsers}></DisplayInfor>
       </div>
     );
   }
