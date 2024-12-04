3a0634d3de21ca1b8abed37eacb36482828072d8
Thang Phan <phanthq2k2@gmail.com>
2024-12-04 15:40:21 +0700

    feat(quiz/question): custom res with lodash - custom ques+ans of quiz

--- src/components/User/QuizDetails.js
+++ src/components/User/QuizDetails.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getQuestionByQuiz } from "../../services/apiServices";
+import _ from "lodash";
 
 const QuizDetails = (props) => {
   const param = useParams();
@@ -13,6 +14,33 @@ const QuizDetails = (props) => {
   const getQuestionData = async () => {
     const res = await getQuestionByQuiz(quizId);
     console.log(res);
+    if (res && res.EC === 0) {
+      let raw = res.DT;
+      let data = _.chain(raw)
+        // Group the elements of Array based on `color` property
+        .groupBy("id")
+        // `key` is group's name (color), `value` is the array of objects
+        .map((value, key) => {
+          let answers = [];
+          let questionDescription,
+            image = null;
+          value.forEach((item, index) => {
+            if (index === 0) {
+              questionDescription = item.description;
+              image = item.image;
+            }
+            answers.push(item.answers);
+          });
+          return {
+            questionId: key,
+            answers,
+            questionDescription,
+            image,
+          };
+        })
+        .value();
+      console.log(data);
+    }
   };
 
   return <div>details</div>;
